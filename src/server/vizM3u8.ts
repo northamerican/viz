62ca0fa95bb01d84080ad8fb3e9ea6d2600d7935
Chris Bitsakis <c.bitsakis@gmail.com>
2023-12-26 21:12:41 -0800

    Update vizM3u8.ts

--- src/server/vizM3u8.ts
+++ src/server/vizM3u8.ts
@@ -9,15 +9,8 @@ const m3u8Template: M3u8Template = ({ QueuesDb, now, longestSegmentDuration, med
 #EXT-X-MEDIA-SEQUENCE:${mediaSequence}
 #EXT-X-START:TIME-OFFSET=${(now - QueuesDb.startTime) / 1000}`
 
-// way with more flexibility (as is) which is simpler and may allow seeking to any point:
-// setting time offset and not having earliestSegmentTime
-// so all segments just accumulate in the m3u8 and never clear.
 // TODO check performance of this fn and see if caching is needed when there's lots of clips
 
-// previous way that works but would complicate seeking:
-// set #EXT-X-START:TIME-OFFSET=0 and 
-
-const streamWindowTimeBefore = 0
 const streamWindowTimeAfter = 45000
 
 export async function vizM3u8() {
@@ -40,10 +33,8 @@ export async function vizM3u8() {
     const timeOffset = QueuesDb.startTime + (1000 * totalDuration);
     totalDuration += duration
 
-    const earliestSegmentTime = now - streamWindowTimeBefore
     const latestSegmentTime = now + streamWindowTimeAfter
     const excludeSegment = latestSegmentTime < timeOffset
-    // const excludeSegment = earliestSegmentTime > timeOffset || latestSegmentTime < timeOffset
 
     if (excludeSegment) return ''
 
@@ -51,11 +42,9 @@ export async function vizM3u8() {
     mediaSequence = Math.min(mediaSequence, playlistSegmentIndex)
 
     const discontinuity = segmentIndex === 0 ? '\n#EXT-X-DISCONTINUITY' : ''
-    // const programDateTime = `#EXT-X-PROGRAM-DATE-TIME:${new Date(timeOffset).toISOString()}`;
     const infDuration = `#EXTINF:${duration.toFixed(6)}`;
     const tsUrl = tsPath(videoId, segmentIndex);
 
-    // return [discontinuity, programDateTime, infDuration, url].join('\n');
     return [discontinuity, infDuration, tsUrl].join('\n');
   }).join('')
 
