9cf4d282ba2bfad8c96b3754fca4499dd2342ad6
Chris Bitsakis <c.bitsakis@gmail.com>
2024-08-16 16:18:27 -0400

    listitem style

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -5,28 +5,3 @@
     <slot />
   </div>
 </template>
-
-<style scoped>
-.list-item {
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  min-height: 64px;
-
-  &.compact {
-    min-height: 40px;
-  }
-
-  & .actions {
-    display: none;
-  }
-
-  &:hover {
-    background: rgba(0, 0, 0, 15%);
-
-    & .actions {
-      display: block;
-    }
-  }
-}
-</style>
