6ea4212916690c206073527277606c7554e496f4
Chris Bitsakis <c.bitsakis@gmail.com>
2024-10-14 00:53:11 -0400

    Update vizEventListener.ts

--- src/vizEventListener.ts
+++ src/vizEventListener.ts
@@ -2,9 +2,11 @@ import { eventsPath, serverEventNames } from "./consts";
 import { store } from "./store";
 import type { VizStoreMethods } from "./types/VizStore";
 
-const serverEventMap: {
+type ServerEventMap = {
   [EventName in (typeof serverEventNames)[number]]: keyof VizStoreMethods;
-} = {
+};
+
+const serverEventMap: ServerEventMap = {
   "accounts:write": "updateAccounts",
   "store:write": "updateSettings",
   "queues:write": "updateQueues",
@@ -13,8 +15,8 @@ const serverEventMap: {
 
 const eventSource = new EventSource(eventsPath);
 
+// Listen for server events, call store method
 export function vizEventListener() {
-  // Listen for server events, call store method
   Object.entries(serverEventMap).map(([eventName, storeMethod]) => {
     eventSource.addEventListener(eventName, () => store[storeMethod]());
   });
