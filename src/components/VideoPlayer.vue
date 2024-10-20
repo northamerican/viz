0437067e02f2e144524360a4f9ecd3c181103ba4
Chris Bitsakis <c.bitsakis@gmail.com>
2024-10-19 21:13:38 -0400

    VideoPlayer fit

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -61,68 +61,79 @@ onMounted(async () => {
 
 <template>
   <section class="player">
-    <video
-      id="video"
-      :ref="(el) => (store.videoEl = el as HTMLMediaElement)"
-      :src="m3u8Path"
-      controls
-      playsinline
-    />
-    <div class="controls">
-      <button @click="playPause">{{ isPlaying ? "⏸" : "▶" }}</button>
-      <small class="time-display time-display--current">{{
-        currentTimeDisplay
-      }}</small>
-      <input
-        class="seeker"
-        type="range"
-        :value="currentTime"
-        min="0"
-        step="0.1"
-        :max="totalDuration"
-        @input="seekTo"
+    <div class="video">
+      <video
+        id="video"
+        :ref="(el) => (store.videoEl = el as HTMLMediaElement)"
+        :src="m3u8Path"
+        playsinline
       />
-      <small class="time-display time-display--total">{{
-        totalDuration || "-"
-      }}</small>
-      <button ref="airPlayButton" hidden>AirPlay</button>
-      <button @click="toggleTv" v-if="tvState !== null">
-        {{ tvState ? "🌝" : "🌚" }}
-      </button>
+      <div class="controls">
+        <button @click="playPause">{{ isPlaying ? "⏸" : "▶" }}</button>
+        <small class="time-display time-display--current">{{
+          currentTimeDisplay
+        }}</small>
+        <input
+          class="seeker"
+          type="range"
+          :value="currentTime"
+          min="0"
+          step="0.1"
+          :max="totalDuration"
+          @input="seekTo"
+        />
+        <small class="time-display time-display--total">{{
+          totalDuration || "-"
+        }}</small>
+        <button ref="airPlayButton" hidden>AirPlay</button>
+        <button @click="toggleTv" v-if="tvState !== null">
+          {{ tvState ? "🌝" : "🌚" }}
+        </button>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped>
 .player {
-  flex-grow: 1;
-  text-align: center;
-
-  #video {
-    width: auto;
-    height: 40vh;
-    max-height: 40vh;
-  }
-}
-
-.controls {
   display: flex;
-  align-items: center;
   justify-content: center;
-  height: 24px;
-  gap: 10px;
-  .seeker {
-    width: 400px;
+  text-align: center;
+  margin: 0 auto;
+
+  .video {
+    display: flex;
+    flex-direction: column;
+
+    video {
+      display: flex;
+      width: auto;
+      max-height: 40vh;
+      min-height: 260px;
+      aspect-ratio: 16 / 9;
+    }
   }
-  .time-display {
-    display: block;
-    width: 4em;
-  }
-  .time-display--current {
-    text-align: right;
-  }
-  .time-display--total {
-    text-align: left;
+
+  .controls {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 24px;
+    gap: 10px;
+
+    .seeker {
+      flex-grow: 1;
+    }
+    .time-display {
+      display: block;
+      width: 4em;
+    }
+    .time-display--current {
+      text-align: right;
+    }
+    .time-display--total {
+      text-align: left;
+    }
   }
 }
 </style>
