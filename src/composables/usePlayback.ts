87c7f6aa189bd347f0fbc682c798c477203f082d
Chris Bitsakis <c.bitsakis@gmail.com>
2025-04-13 01:10:41 -0700

    currentQueueItem fix

--- src/composables/usePlayback.ts
+++ src/composables/usePlayback.ts
@@ -64,7 +64,7 @@ export function usePlayback() {
     const queueItems = activeQueue.value.items;
 
     return queueItems.find((item) => {
-      accumulatedTime += item.video?.duration;
+      accumulatedTime += item.video?.duration || 0;
       return currentVideoTime.value <= accumulatedTime;
     });
   });
